03871391429da781e4d371dc0036fe2c7b0fe274
Ikonera <61968829+Ikonera@users.noreply.github.com>
2022-02-21 16:40:30 +0100

    feat(#2): add `about page (#15)
    Co-authored-by: Gabriel MILLET <gabriel.millet@42c.fr>

---
+++ pages/about.tsx
@@ -0,0 +1,16 @@
+import { NextPage } from "next";
+
+const About: NextPage = () => {
+    return (
+        <section>
+            <div>
+                <h2>A Propos</h2>
+            </div>
+            <div>
+                Salut et bienvenue dans la page `A Propos`
+            </div>
+        </section>
+    )
+}
+
+export default About
