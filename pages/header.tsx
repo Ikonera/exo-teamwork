c17e566f4f618ad7b7af18a5205f643ece554291
Mohamed <mohamed-makhtar.mbengue@estiam.com>
2022-02-21 12:23:23 +0100

    feat(#3): add Header

---
+++ pages/header.tsx
@@ -0,0 +1,11 @@
+import { FunctionComponent } from "react";
+
+const Header: FunctionComponent =() =>{
+    return (
+        <Header>
+        <h1>Home Header</h1>
+        </Header>
+    )
+}
+
+export default Header 
