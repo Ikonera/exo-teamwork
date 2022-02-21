3fce41d7d52679f4d660c757d1b57704e759744e
pierrelequemener <98084856+pierrelequemener@users.noreply.github.com>
2022-02-21 16:36:25 +0100

    feat (#1) add footer page (#13)

---
+++ pages/footer.tsx
@@ -0,0 +1,11 @@
+import { FunctionComponent } from "react";
+
+const Footer : FunctionComponent = () => {
+   return (
+    <section>
+        <div id="pageFooter-last">Footer on last page</div>
+    </section>
+   )
+}
+
+export default Footer 
